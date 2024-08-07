209ed53d6490921b5dcce8591930a39bfb33ded2
argvchs <argvchs@outlook.com>
2024-08-07 23:42:41 +0800

    chore: format code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { transformSync } = require("@babel/core");
 const { isMatch } = require("micromatch");
 hexo.extend.renderer.register("js", "js", ({ path, text }) => {
-    let { options = {}, exclude = [] } = hexo.config.babel;
+    const { options = {}, exclude = [] } = hexo.config.babel;
     if (isMatch(path, exclude, { basename: true })) return text;
     return transformSync(text, options).code;
 });
